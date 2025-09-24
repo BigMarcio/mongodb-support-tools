ef6f519550860ea2b09ccfef6bcd2d8041c62087
Márcio Ribeiro <marcio.ribeiro@mongodb.com>
2025-09-24 17:22:40 -0300

    List 10 documents in natural order from the collections with non ObjectId _id

--- migration/mongodb_id_checker/mongodb_id_checker.js
+++ migration/mongodb_id_checker/mongodb_id_checker.js
@@ -1,97 +1,66 @@
-printjson('-----------------------------------------------------------');
-printjson('Helper tool to identify type of _id on user collections');
-printjson('-----------------------------------------------------------');
-
-printjson(" ")
+print("namespace,type,count");  
+  
 const systemDBs = ["admin", "config", "local"];  
-db.getMongo().getDBNames().forEach(function (d) {
-    if (systemDBs.includes(d)) {  
-        return; // Skip system DBs  
+const typesMap = {  
+    "1": "Double",  
+    "2": "String",  
+    "3": "Object",  
+    "5": "Binary data",  
+    "6": "Undefined",  
+    "7": "ObjectId",  
+    "8": "Boolean",  
+    "9": "Date",  
+    "10": "Null",  
+    "11": "Regular Expression",  
+    "12": "DBPointer",  
+    "13": "Javascript",  
+    "14": "Symbol",  
+    "15": "Javascript code w. scope",  
+    "16": "32-bit integer",  
+    "18": "64-bit integer",  
+    "19": "Decimal128",  
+    "-1": "Min key",  
+    "127": "Max key"  
+};  
+// Note: type "7" (ObjectId) is intentionally omitted from typesMap  
+  
+let nonObjectIdNamespaces = [];  
+  
+db.getMongo().getDBNames().forEach(function (dbName) {  
+    if (systemDBs.includes(dbName)) {  
+        return;  
     }  
-    const curr_db = db.getMongo().getDB(d);  
-
-    curr_db.getCollectionNames().forEach(function (coll) {
-        var count = -1
-        printjson('==============================================');
-        printjson('= Namespace: ' + d + '.' + coll );
-        printjson('==============================================');
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 7 } } )
-        if (count > 0) {
-            printjson(count+" * ObjectId ")
-        }
-        printjson('   Other types:');
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 1 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Double ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 2 } } )
-        if (count > 0) {
-            printjson('   '+count+"       String ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 3 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Object ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 5 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Binary data ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 6 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Undefined ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 8 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Boolean ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 9 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Date ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 10 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Null ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 11 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Regular Expression ",)
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 12 } } )
-        if (count > 0) {
-            printjson('   '+count+"       DBPointer ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 13 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Javascript ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 14 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Symbol ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 15 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Javascript code ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 16 } } )
-        if (count > 0) {
-            printjson('   '+count+"       32-bit integer ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 18 } } )
-        if (count > 0) {
-            printjson('   '+count+"       64-bit integer ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 19 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Decimal128 ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : -1 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Min key ")
-        }
-        count = curr_db.getCollection(coll).countDocuments( { "_id" : { $type : 127 } } )
-        if (count > 0) {
-            printjson('   '+count+"       Max key ")
-        }   
-
-    });
-});
+    const curr_db = db.getMongo().getDB(dbName);  
+  
+    curr_db.getCollectionNames().forEach(function (collName) {  
+        let hasNonObjectId = false;  
+        Object.keys(typesMap).forEach(function(typeNum) {  
+            var count = curr_db.getCollection(collName).countDocuments({"_id": { $type: Number(typeNum) }});  
+            if (count > 0) {   
+                print([dbName + "." + collName, typesMap[typeNum], count].join(","));  
+                    if (Number(typeNum) != 7) {
+                        hasNonObjectId = true;
+                    }
+            }  
+        });  
+        // If any _id is not ObjectId, track the namespace for second step  
+        if (hasNonObjectId) {  
+            nonObjectIdNamespaces.push({ dbName: dbName, collName: collName });  
+        }  
+    });  
+});  
+  
+// Second phase: list the first 10 _id for those namespaces  
+print("\nNamespaces with at least one non-ObjectId _id:\n");  
+  
+nonObjectIdNamespaces.forEach(function(ns) {  
+    const curr_db = db.getMongo().getDB(ns.dbName);  
+    print("Namespace: " + ns.dbName + "." + ns.collName);  
+    curr_db.getCollection(ns.collName)  
+        .find({}, { _id: 1 })  
+        .sort({ $natural: 1 })  
+        .limit(10)  
+        .forEach(function (doc) {  
+            printjson(doc);  
+        });  
+});  
